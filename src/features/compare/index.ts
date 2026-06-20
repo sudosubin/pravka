fb00c8d5e85fef3b25843dff18643693f707af6b
sudosubin <sudosubin@gmail.com>
2026-06-21 00:15:19 +0900

    feat(compare): add barrel exports

---
+++ src/features/compare/index.ts
@@ -0,0 +1,4 @@
+export { calibrateCoverage } from "@/features/compare/calibrate.ts";
+export { runCharsCompare } from "@/features/compare/chars.ts";
+export { buildDocsCompare } from "@/features/compare/docs.ts";
+export { renderReport } from "@/features/compare/report.ts";
